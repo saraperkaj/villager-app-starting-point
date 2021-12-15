26b13ee3ff4aec4dfe7102660e742e848cdd6ac4
Sara Perkaj <saraperkaj@pursuit.org>
2021-12-15 15:47:51 -0500

    not done yet.

--- src/App.js
+++ src/App.js
@@ -9,20 +9,63 @@ class App extends Component {
     this.state = {
       villagerData: villagerData,
       villagers: [],
+      currentVillager: villagerData.ant00,
+      showVillage: true,
     };
   }
+  changeVillager = () => {
+    const { villagerData } = this.state;
+    const numVillagers = Object.keys(this.state.villagerData).length;
+    const randomI = Math.floor(Math.random() * numVillagers);
+    const randomVillager = Object.values(villagerData)[randomI];
+    this.setState({ currentVillager: randomVillager });
+  };
+  addVillager = () => {
+    const { villagers, currentVillager } = this.state;
+    this.setState({ villagers: [...villagers, currentVillager] });
+  };
+
+  clearVillage = () => {
+    this.setState({ villagers: [] });
+  };
+
+  toggleVillageHiding = () => {
+    if (this.setState.showVillage === true) {
+      this.setState({ showVillage: false });
+    } else {
+      this.setState({ showVillage: false });
+    }
+  };
+  // const handleSubmit = (event) => {
+  //   const { villagers, villagerData } = this.state;
+  //   event.preventDefault();
+  //   this.setState({
+  //     villagers: [...villagers, { villagerData: villagerData }],
+  //   });
+  // };
 
   render() {
+    const { currentVillager, villagers, showVillage } = this.state;
+
+    const names = villagers.map((villager) => (
+      <p key={villager.id}>{villager.name["name-USen"]}</p>
+    ));
     return (
       <div className="App">
         <h1>Animal Crossing Villagers</h1>
         <div>
-          <button>Change villager</button>
-          <button>Add villager to village</button>
-          <button>Clear Village</button>
-          <div>Currently selected villager:</div>
-          <div>Number of villagers in village: </div>
-          <div className="villager-grid"></div>
+          <button onClick={this.changeVillager}>Change villager</button>
+          <button onClick={this.addVillager}>Add villager to village</button>
+          <button onClick={this.clearVillage}>Clear Village</button>
+          <div>
+            Currently selected villager: {currentVillager.name["name-USen"]}
+          </div>
+          <div>
+            Number of villagers in village:{" "}
+            {villagers.length ? villagers.length : "none yet!"}
+          </div>
+          <button onClick={this.toggleVillageHiding}>Show/Hide Village</button>
+          <div className="villager-grid">{showVillage ? names : ""}</div>
         </div>
       </div>
     );
